768ff519fe6550c5e2db0787953c8969ce56a292
Andres Felipe Vela <38870504+andrespick@users.noreply.github.com>
2025-10-05 15:13:56 -0500

    se agrega la pagina de recuperar contraseña

--- View/recoverP.php
+++ View/recoverP.php
@@ -4,26 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="Styles/style_recoverP.css">
+  <link rel="shortcut icon" href="../img/logo.svg" />
   <title>Recuperar Contraseña</title>
 </head>
 <body>
- 
   <div class="container">
-    <img src="../img/logo.png" alt="logo" class="logo">
- 
-    <div class="form-container">
+    <div class="card">
+      <img src="../img/logo.png" alt="Logo Aphia" class="logo">
       <h1 class="title">Recuperación Contraseña</h1>
-      <form method="POST" action="ASIGNAR RUTA PARA RECUPERAR CONTRASEÑA">
+
+      <form method="POST" action="ASIGNAR_RUTA_PARA_RECUPERAR_CONTRASEÑA">
         <input
           type="email"
           name="email"
           placeholder="Introduce tu dirección de correo electrónico:"
-          class="username"
+          class="email-input"
           required>
         <button type="submit" class="reset-btn">Restablecer Contraseña</button>
       </form>
     </div>
   </div>
- 
 </body>
 </html>
